4ea80cab319afabedf8dd003be97bd9a1baadd97
KotOwl <stas.black31@gmail.com>
2025-03-23 21:37:00 +0200

    start

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1 className='border-8 border-inherit'>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
