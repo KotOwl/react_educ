139b182b89767a5d12f2b454c71daaa145acf413
KotOwl <stas.black31@gmail.com>
2025-03-25 22:02:26 +0200

    start

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
 import './App.css'
-import {PostsComponent} from "./Components/postsComponent/PostsComponent.tsx";
 
 function App() {
 
 
   return (
     <>
-      <PostsComponent/>
     </>
   )
 }
--- src/service/posts.API.ts
+++
@@ -1,8 +0,0 @@
-import {IPosts} from "../models/IPosts.ts";
-
-const fetchPosts = async ():Promise<IPosts[]>=> {
-    return await fetch('https://jsonplaceholder.typicode.com/posts')
-        .then(value => value.json())
-}
-
-export {fetchPosts}
